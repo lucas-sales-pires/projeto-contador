f10194825d9b9c532e31650704c6b6d71a79a19f
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-05-21 09:10:25 -0300

    meu primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# contador_de_pessoas
+
+Contador de pessoas
+
+## Imagem Aplicativo
+
+<img src="imagem_aplicativo.png">
---
+++ android/app/src/main/kotlin/br/vet/fabio/contador_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.vet.fabio.contador_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
